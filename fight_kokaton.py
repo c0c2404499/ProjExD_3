330a93da244301c9a619fa9005e71586a6a8e019
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-29 15:03:35 +0900

    練習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,7 @@ def main():
                 if beam.rct.colliderect(bomb.rct):
                     beam = None #ビームを消す
                     bomb = None #爆弾を消す
+                    bird.change_img(0, screen) #よろこびエフェクト
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
